d04512119b732205399fcb8aa362d437e52151d7
Kamil Jarzab <kamil.jarzab@cravtt.com>
2017-08-07 14:45:46 +0200

    Router get now send url

--- Timestamp microservice/timeController.js
+++ Timestamp microservice/timeController.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
+var url = require('url');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post("/", function(req, res){
+router.post("*", function(req, res){
   res.end("test");
 })
 
-router.get("/", function(req, res){
-  res.send("test");
+router.get("*", function(req, res){
+  res.send(url.parse(req.url, true));
 })
 
 module.exports = router;
